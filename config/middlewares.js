80aa0b4d506f215e3ea1f9a5b739ce164ff7084b
Doglidev <doglianimateo@gmail.com>
2025-05-15 18:59:35 -0300

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,18 @@
 module.exports = [
-  "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
   'strapi::session',
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::favicon',
+  {
+    name: 'strapi::static',
+    config: {
+      path: 'public',  // acá le decís que la carpeta pública está en la raíz /public
+    },
+  },
+  'strapi::public',
 ];
